a61d82d2fb8e8f0625c2ae7c324030cbdbee33c8
PandaHunterX <147257813+PandaHunterX@users.noreply.github.com>
2023-12-22 01:38:51 +0800

    My First Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var Slideshow = document.getElementById('image');
+var imageIndex = 0;
+var imageArray = ['assets/Image4.jpg', 'assets/Image2.jpg', 'assets/Image3.jpg', 'assets/Image1.jpg'];
+var isAnimating = false;
+
+function changeImage() {
+    if (isAnimating) {
+        return;
+    }
+
+    isAnimating = true;
+    
+    Slideshow.style.animation = 'fadeOut 0.5s ease-in-out';
+    
+    Slideshow.addEventListener('animationend', function () {
+        Slideshow.src = imageArray[imageIndex];
+        Slideshow.style.animation = 'fadeIn 0.5s ease-in-out';
+        imageIndex = (imageIndex + 1) % imageArray.length;
+        isAnimating = false;
+    }, { once: true });
+}
+
+var intervalId = setInterval(changeImage, 5000);
