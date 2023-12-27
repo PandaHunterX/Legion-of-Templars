aeacdaa9003d886baa7085f7df877bde4a9451aa
PandaHunterX <147257813+PandaHunterX@users.noreply.github.com>
2023-12-28 00:07:55 +0800

    Index Page Complete

--- script.js
+++ script.js
@@ -12,13 +12,40 @@ function changeImage() {
     
     Slideshow.style.animation = 'fadeOut 0.5s ease-in-out';
     
-    Slideshow.addEventListener('animationend', function () {
+    Slideshow.addEventListener('animationend', () => {
         Slideshow.src = imageArray[imageIndex];
         Slideshow.style.animation = 'fadeIn 0.5s ease-in-out';
         imageIndex = (imageIndex + 1) % imageArray.length;
         isAnimating = false;
     }, { once: true });
 }
+
+let Submit = document.getElementById('contactsubmit');
+
+Submit.addEventListener('click', () => {
+    var Fname = document.getElementsByName('F_name')[0].value;
+    var Lname = document.getElementsByName('L_name')[0].value;
+    var email = document.getElementsByName('email')[0].value;
+    var number = document.getElementsByName('phone')[0].value;
+    var comment = document.getElementsByName('comment')[0].value;
+    document.getElementById('warning').style.display = 'none';
+    document.getElementsByClassName('mainform')[0].style.animation = 'none';
+
+    if (Fname === '' || Lname === '' || email === '' || number === '' || comment === '') {
+        document.getElementById('warning').style.display = 'block'
+        document.getElementsByClassName('mainform')[0].style.animation = 'shake linear 1s forwards'
+    }
+    else {
+        document.getElementsByClassName('popupbox')[0].style.display = 'grid'
+        document.getElementsByClassName('popupbox')[0].style.animation = 'entrance linear 0.2s forwards'
+    }
+})
+
+let Close = document.getElementById('close');
+
+Close.addEventListener('click', () => {
+    document.getElementsByClassName('popupbox')[0].style.animation = 'close linear 0.2s forwards'
+})
 
 setInterval(changeImage, 3000);
 
